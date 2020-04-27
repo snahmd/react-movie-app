da0f8009a28763e40375c37391191e2cae32ce2e
snahmd <sanahmed7@gmail.com>
2020-04-27 23:55:25 +0200

    adding redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(
+  rootReducer
+);
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
